ab464db628b5fc78cf8fa947f1991154b1ebc685
Julian <julianaguero@gmail.com>
2023-11-15 16:21:30 +0100

    misspeling fix

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -28,7 +28,7 @@ export const sendEmail = async (formData: FormData) => {
     let data;
     try {
         data = await resend.emails.send({
-            from: "Contact Form <onboarding@resend.>",
+            from: "Contact Form <onboarding@resend.dev>",
             to: ["julianaguero@gmail.com"],
             subject: "Message from contact form",
             reply_to: senderEmail as string,
